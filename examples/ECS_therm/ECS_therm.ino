f89c0032afd77e7428f8de2855ab416f70c7ce92
Sylvain Centelles <scentelles@gmail.com>
2017-01-15 21:27:37 +0100

    Remove FORCE message, as it will be handled on server side. And add relay control

--- examples/ECS_therm/ECS_therm.ino
+++ examples/ECS_therm/ECS_therm.ino
@@ -5,10 +5,13 @@ MqttConnection * myMqtt;
 #include <DallasTemperature.h>
 
 //Constants
-#define SENSOR_ID "ECS"
-#define PROBE_TEMPO 60000
+const char ECS_STATE_OFF = '1';
+const char ECS_STATE_ON  = '2';
+
+#define SENSOR_ID "ECS2"
+#define PROBE_TEMPO 60000  //1 measure per minute
 #define ONE_WIRE_BUS D4  // DS18B20 pin
-#define RELAY_PIN D2
+#define RELAY_PIN D1
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature DS18B20(&oneWire);
 
@@ -25,80 +28,76 @@ DallasTemperature DS18B20(&oneWire);
 #define MQTT_SERVER  "192.168.1.27"
 #define MQTT_PORT 1883
 
-#define ECS_FORCE_DISABLED 	'0'
-#define ECS_FORCE_OFF 		'1'
-#define ECS_FORCE_ON  		'2'
-#define ECS_STATE_ON		'2'
-#define ECS_STATE_OFF		'1'
-
+#define DELAY_BETWEEN_TOGGLE_ON 9000 //00  //15min
+#define DELAY_BETWEEN_TOGGLE_OFF 900 //000  //15min
 
 long lastMsg = 0;
-char ecsForce = ECS_FORCE_DISABLED;
 char ecsState = ECS_STATE_OFF;
 char msg[2];
+unsigned long lastSwitchOn = 0;
+unsigned long lastSwitchOff = 0;
+
+bool checkToggleTooFast(bool switchOn){
+  Serial.print("now :");
+  Serial.println(millis());
+  Serial.print("last on:");
+  Serial.println(lastSwitchOn);  
+  Serial.print("last off:");
+  Serial.println(lastSwitchOff);   
+  if(switchOn){
+      if(millis() - lastSwitchOff < DELAY_BETWEEN_TOGGLE_ON)
+         return true;
+      else
+         return false;
+   }
+   else{
+      if(millis() - lastSwitchOn < DELAY_BETWEEN_TOGGLE_OFF)
+         return true;
+      else
+         return false;
+  
+   
+   }
+      
+}
 
 void processEcsStateMsg(char* topic, byte* payload, unsigned int length)
 {
-	
+
   Serial.print("Checking if state topic");
-  if(String(topic) == "ECS/state"){
+ // if(String(topic) == "ECS/state"){
 	  Serial.println("Received ECS/State change message");
-	  if(ecsForce == ECS_FORCE_DISABLED)
-	  {
 		 Serial.println("changing ECS relay state");
 	     if ((char)payload[0] == ECS_STATE_ON) {
 		    Serial.println("ECS ON received");
-			ecsState = ECS_STATE_ON;
+						
+			if(!checkToggleTooFast(true)){
+			    ecsState = ECS_STATE_ON;
+			    digitalWrite(RELAY_PIN, HIGH);
+				lastSwitchOn = millis();
+			}
+		    else
+			    Serial.println("Skipping, switching too fast");
 	     }
-	     else{
+	     else if ((char)payload[0] == ECS_STATE_OFF){
 		    Serial.println("ECS OFF received");
-			ecsState = ECS_STATE_OFF;
-	     }
-     }
-	  else
-	 {
-			Serial.println("FORCED is set. No action on relay. Rolling back state on MQTT server");
-			//Rewrite current state only if delta with current state. Else we go into infinite loop...
-			  if(ecsState != (char)payload[0])
-			  {       
-		              msg[0] = ecsState;
-					  msg[1] = 0;
-					  myMqtt->publish("ECS/state", msg);
-			  }
-	 }
-	  
-  }
-  else if(String(topic) == "ECS/force"){
-	  Serial.println("Received ECS/State change message");
-	  
-	  if ((char)payload[0] == ECS_FORCE_OFF) {
-		Serial.println("Force OFF received");
-	    msg[0] = ECS_STATE_OFF;
-		msg[1] = 0;
-		myMqtt->publish("ECS/state", msg);
-		//TODO : ugly
-	    delay(1000);
-		ecsForce = ECS_FORCE_OFF;
 
-	  }
-	  else if ((char)payload[0] == ECS_FORCE_ON) {
-		Serial.println("Force ON received");
-		msg[0] = ECS_STATE_ON;
-		msg[1] = 0;
-		myMqtt->publish("ECS/state", msg);
-		//TODO : ugly
-		delay(1000);
-		ecsForce = ECS_FORCE_ON;
-	  }
-	  else
-	  {
-		  Serial.println("Force disabled");
-		  ecsForce = ECS_FORCE_DISABLED;
-	  }
-  }
-  else {
-	  Serial.println("not for me...");
-  }  
+			if(!checkToggleTooFast(false)){
+			   ecsState = ECS_STATE_OFF;
+			   digitalWrite(RELAY_PIN, LOW);
+			   lastSwitchOff = millis();
+			}
+	     }
+		 else {
+		    Serial.print("Unknown payload : ");
+			Serial.println((char)payload[0]);
+		 }
+
+	  
+  //}
+  //else {
+  //	  Serial.println("not for me...");
+  //}  
 
 	
 }
@@ -106,15 +105,22 @@ void processEcsStateMsg(char* topic, byte* payload, unsigned int length)
 
 void setup() {
   Serial.begin(115200);
+  pinMode(RELAY_PIN, OUTPUT);
+  digitalWrite(RELAY_PIN, LOW);
+
+  
   myMqtt = new MqttConnection(SENSOR_ID, WLAN_SSID, WLAN_PASS, MQTT_SERVER, MQTT_PORT);
   myMqtt->registerCustomProcessing(&processEcsStateMsg);
   myMqtt->addSubscription("state");
   myMqtt->addSubscription("force");  
+  
+  
 }
 
 void loop() {
-  float temp;
+  float temp1, temp2;
   if (!myMqtt->connected()) {
+	Serial.println("still not connected, retrying");
     myMqtt->reconnect();
   }
   myMqtt->loop();
@@ -127,17 +133,20 @@ void loop() {
       
       //Read data and store it to variables hum and temp
       DS18B20.requestTemperatures(); 
-      temp = DS18B20.getTempCByIndex(0);
+      temp1 = DS18B20.getTempCByIndex(0);
       Serial.print("Temperature: ");
-      Serial.println(temp);
-      
-      if (temp == 85.0 || temp == (-127.0)) {
+      Serial.println(temp1);
+      temp2 = DS18B20.getTempCByIndex(1);
+      Serial.print("Temperature: ");
+      Serial.println(temp2);      
+      if (temp1 == 85.0 || temp1 == (-127.0) || temp2 == 85.0 || temp2 == (-127.0)) {
           Serial.println("Failed to read from sensor!");
       }
       else
       {
           //send to MQTT server
-          myMqtt->publishValue("temp", temp, 1);    
+          myMqtt->publishValue("temp", temp1, 1);    
+		  myMqtt->publishValue("temp", temp2, 1);    
       }
   }
 
@@ -145,4 +154,3 @@ void loop() {
   
 
 }
-
