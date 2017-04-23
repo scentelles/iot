90411a75cee798f8cb367063282ef2683a58d81f
scentelles <scentelles@gmail.com>
2017-04-23 17:46:51 +0200

    update domoticz indexes and and moist value

--- server/domoticz_mqtt.py
+++ server/domoticz_mqtt.py
@@ -7,26 +7,44 @@ def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
     mqttClient.subscribe("domoticz/out")
     mqttClient.subscribe("ROOM1_SENSOR/temp")
-
+    mqttClient.subscribe("ECS/temp1")
+    mqttClient.subscribe("ECS/temp2")
+    mqttClient.subscribe("FLOWER1_WATER/moisture")
 
     
 def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload)+"\n")
+   # print(msg.topic+" "+str(msg.payload)+"\n")
     
     if msg.topic == "ROOM1_SENSOR/temp":
-        value = "{ 'idx' : 4, 'nvalue' : 0, 'svalue' : '" + msg.payload +"'}"
-        mqttClient.publish("domoticz/in", payload=msg.payload)
+        value = '{ "idx" : 4, "nvalue" : 0, "svalue" : "' + msg.payload + '"}'
+        mqttClient.publish("domoticz/in", payload=value)
+
+    if msg.topic == "ECS/temp1":
+        value = '{ "idx" : 1, "nvalue" : 0, "svalue" : "' + msg.payload + '"}'
+        mqttClient.publish("domoticz/in", payload=value)
+    if msg.topic == "ECS/temp2":
+        value = '{ "idx" : 2, "nvalue" : 0, "svalue" : "' + msg.payload + '"}'
+        mqttClient.publish("domoticz/in", payload=value)
+
+    if msg.topic == "FLOWER1_WATER/moisture":
+        print ("received moisture update")
+        value = '{ "idx" : 27, "nvalue" : 0, "svalue" : "' + msg.payload + '"}'
+        mqttClient.publish("domoticz/in", payload=value)
+        value = '{ "idx" : 28, "nvalue" : 0, "svalue" : "' + msg.payload + '"}'
+        mqttClient.publish("domoticz/in", payload=value)
+
+
     
     if msg.topic == "domoticz/out":
-        print msg.topic
+       # print msg.topic
    # print msg.payload
         data = json.loads(msg.payload)
         index = data["idx"]
-        print index
+      #  print index
         nvalue = data["nvalue"]
-        print nvalue
+      #  print nvalue
         name = data["name"]
-        print name
+      #  print name
         topic = name + "/command"
         if name == "dreamroom/chauffage":
             print "Sending command to MQTT broker on topic :"
