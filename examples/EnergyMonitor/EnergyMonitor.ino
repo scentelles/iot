996fd368473fe78e00bddfd1417fd29abd626a9c
scentelles <scentelles@gmail.com>
2017-08-03 19:18:53 +0200

    update to have less probes, and only one per loop

--- examples/EnergyMonitor/EnergyMonitor.ino
+++ examples/EnergyMonitor/EnergyMonitor.ino
@@ -17,9 +17,13 @@ MqttConnection * myMqtt;
 
 #define SENSOR_ID "EDF"
 
+#define PROBE_DELAY 10000  //10 seconds
+#define ENERGY_VS_POWER_PROBE_RATIO 6
 
 PZEM004T pzem(D2,D1);
 IPAddress ip(192,168,1,1);
+
+int count = 0;
 
 void setup() {
   Serial.begin(115200);
@@ -35,42 +39,46 @@ void loop() {
   }
   myMqtt->loop();
 
+  count ++;
   
-  float v = pzem.voltage(ip);
+/*  float v = pzem.voltage(ip);
   wdt_reset();
   if (v < 0.0) v = 0.0;
   Serial.print(v);Serial.print("V; ");
   myMqtt->publishValue("voltage", (int)v, 1);
+*/
 
-
-
-
-
-
-  float i = pzem.current(ip);
+/*  float i = pzem.current(ip);
   wdt_reset();
   if(i >= 0.0){ 
     Serial.print(i);Serial.print("A; "); 
     myMqtt->publishValue("current", i, 1);
   }
-  
-  float p = pzem.power(ip);
-  wdt_reset();
-  if(p >= 0.0){ 
-    Serial.print(p);Serial.print("W; ");
-    myMqtt->publishValue("power", p, 1);
-  }
-  
-  float e = pzem.energy(ip);
-  wdt_reset();
-  if(e >= 0.0){ 
-    Serial.print(e);Serial.print("Wh; "); 
-    myMqtt->publishValue("energy", v, 1);
-  }
+*/
 
+  if (count % ENERGY_VS_POWER_PROBE_RATIO != 0)
+  {
+	  float p = pzem.power(ip);
+	  wdt_reset();
+	  if(p >= 0.0){ 
+		Serial.print(p);Serial.print("W; ");
+		myMqtt->publishValue("power", p, 1);
+	  }
+  }
+  
+  else
+  {
+	  float e = pzem.energy(ip);
+	  wdt_reset();
+	  if(e >= 0.0){ 
+		Serial.print(e);Serial.print("Wh; "); 
+		myMqtt->publishValue("energy", e, 1);
+	  }
+  }
+  
   Serial.println();
 
-  delay(1000);
+  delay(PROBE_DELAY);
 }
 
 
