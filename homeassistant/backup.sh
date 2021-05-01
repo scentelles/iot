6c178c7d2b6fabf1bcf54feae7416f8ea13c584c
Sylvain Centelles <scentelles@gmail.com>
2021-05-01 14:49:29 +0200

    file and script for HA backup

---
+++ homeassistant/backup.sh
@@ -0,0 +1,5 @@
+sudo cp /home/homeassistant/.homeassistant/configuration.yaml .
+sudo cp /home/homeassistant/.homeassistant/.storage/lovelace .storage/
+sudo cp /home/homeassistant/.homeassistant/automations.yaml .
+sudo cp /home/homeassistant/.homeassistant/scripts.yaml .
+sudo cp /home/homeassistant/.homeassistant/scenes.yaml .
