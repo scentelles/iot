331a3735ac4fe8c7f5178159f9b7d0573edd9c65
Sylvain Centelles <scentelles@gmail.com>
2022-04-18 18:55:32 +0200

    new update

--- homeassistant/stop_pc.sh
+++ homeassistant/stop_pc.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-net rpc shutdown -f -t 0 -C 'Extinction du PC !' -I 192.168.1.77 --user Administrateur%totototo
-net rpc shutdown -f -t 0 -C 'Extinction du PC !' -I 192.168.1.65 --user Administrateur%totototo
+net rpc shutdown -f -t 0 -C 'Extinction du PC !' -I 192.168.1.24 --user Administrateur%totototo
+net rpc shutdown -f -t 0 -C 'Extinction du PC !' -I 192.168.1.24 --user Administrateur%totototo
 
