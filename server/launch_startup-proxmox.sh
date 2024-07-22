4d58a63c12bd3aa4541169dfcf550ae2c9a17ed3
Sylvain Centelles <scentelles@gmail.com>
2024-07-22 15:20:16 +0000

    mod

--- server/launch_startup-proxmox.sh
+++ server/launch_startup-proxmox.sh
@@ -11,9 +11,10 @@ echo "startup system mqtt script"
 python3 -u /home/iot/Projects/iot/server/system_mqtt.py > /home/iot/Projects/system_log.txt &
 sleep 1   
 
-echo "startup RF script"
-python3 -u /home/iot/Projects/iot/server/RFbridge.py > /home/iot/Projects/RFbridge.log &
-sleep 1
-
 echo "startup AirC script"
 python3 -u /home/iot/Projects/iot/server/AirC/airc_control.py > /home/iot/Projects/AirC.log &
+
+sleep 1
+
+echo "startup RF script"
+python3 -u /home/iot/Projects/iot/server/RFbridge.py > /home/iot/Projects/RFbridge.log &
